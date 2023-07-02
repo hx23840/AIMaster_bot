723aa2e90317a992fe52ff7322cd4d69cea8a55a
Peter Shi <targe@protonmail.com>
2023-07-02 22:22:55 +0800

    fix clerk url

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,16 @@
 import { authMiddleware } from "@clerk/nextjs";
+import {redirectToSignIn} from '@clerk/nextjs';
 
-export default authMiddleware();
+export default authMiddleware({
+        afterAuth(auth, req, evt) {
+            // handle users who aren't authenticated
+            if (!auth.userId && !auth.isPublicRoute) {
+                return redirectToSignIn({returnBackUrl: "https://aimaster.chat2any.com/"});
+            }
+        }
+    }
+);
+
 
 export const config = {
     matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
