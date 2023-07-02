0eb1e39eebadb3cd0085e9cb7a901c60d944cbc7
Peter Shi <targe@protonmail.com>
2023-07-02 20:44:16 +0800

    add Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,39 +2,43 @@
 import "./styles/globals.scss";
 import "./styles/markdown.scss";
 import "./styles/highlight.scss";
-import { getClientConfig } from "./config/client";
+import {getClientConfig} from "./config/client";
+import {Analytics} from '@vercel/analytics/react';
 
 export const metadata = {
-  title: "AIMaster",
-  description: "Your personal AI Bot.",
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-    maximumScale: 1,
-  },
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "#fafafa" },
-    { media: "(prefers-color-scheme: dark)", color: "#151515" },
-  ],
-  appleWebApp: {
     title: "AIMaster",
-    statusBarStyle: "default",
-  },
+    description: "Your personal AI Bot.",
+    viewport: {
+        width: "device-width",
+        initialScale: 1,
+        maximumScale: 1,
+    },
+    themeColor: [
+        {media: "(prefers-color-scheme: light)", color: "#fafafa"},
+        {media: "(prefers-color-scheme: dark)", color: "#151515"},
+    ],
+    appleWebApp: {
+        title: "AIMaster",
+        statusBarStyle: "default",
+    },
 };
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+                                       children,
+                                   }: {
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta name="config" content={JSON.stringify(getClientConfig())} />
-        <link rel="manifest" href="/site.webmanifest"></link>
-        <script src="/serviceWorkerRegister.js" defer></script>
-      </head>
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <head>
+            <meta name="config" content={JSON.stringify(getClientConfig())}/>
+            <link rel="manifest" href="/site.webmanifest"></link>
+            <script src="/serviceWorkerRegister.js" defer></script>
+        </head>
+        <body>
+          {children}
+          <Analytics/>
+        </body>
+        </html>
+    );
 }
