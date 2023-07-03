b0083d01193b5ac34a4ad19f71f8e67ec369eb3a
Peter Shi <targe@protonmail.com>
2023-07-03 12:40:44 +0800

    fix auth bug

--- middleware.ts
+++ middleware.ts
@@ -2,14 +2,14 @@ import { authMiddleware } from "@clerk/nextjs";
 import {redirectToSignIn} from '@clerk/nextjs';
 
 export default authMiddleware(
-    //{
-    //     afterAuth(auth, req, evt) {
-    //         // handle users who aren't authenticated
-    //         if (!auth.userId && !auth.isPublicRoute) {
-    //             return redirectToSignIn({returnBackUrl: "https://aimaster.chat2any.com/"});
-    //         }
-    //     }
-    // }
+    {
+        afterAuth(auth, req, evt) {
+            // handle users who aren't authenticated
+            if (!auth.userId && !auth.isPublicRoute) {
+                return redirectToSignIn({returnBackUrl: "https://aimaster.chat2any.com/"});
+            }
+        }
+    }
 );
 
 
