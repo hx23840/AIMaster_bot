2d1f94283c49736f844b6762b71174be48578f20
Peter Shi <targe@protonmail.com>
2023-11-20 17:42:35 +0800

    fix redis config error

--- app/api/utils/redisClient.ts
+++ app/api/utils/redisClient.ts
@@ -5,7 +5,7 @@ let redis: Redis | null = null;
 export function getRedisClient(): Redis {
     if (!redis) {
         redis = new Redis({
-            port: 7713,
+            port: Number(process.env.REDISPORT),
             db: 2,
             host: process.env.REDISHOST || 'localhost',
             password: process.env.REDISPASSWORD || '',
